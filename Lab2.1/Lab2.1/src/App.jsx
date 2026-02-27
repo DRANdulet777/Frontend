fbb29c4a7fe11d5f43d7ba4c93e403beb23f38a5
Amirkhanov Daulet <amirkhanov.dauka.azino@mail.ru>
2026-02-27 23:26:52 +0500

    add task3 combinedlist

--- Lab2.1/Lab2.1/src/App.jsx
+++ Lab2.1/Lab2.1/src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from '/vite.svg'
 import FragmentLayout from "./FragmentLayout";
 import './App.css'
 import ItemList from './ItemList';
+import CombinedList from './CombinedList';
 
 function App() {
   return (
     <>
     <FragmentLayout />
     <ItemList />
+    <CombinedList />
     </>
   );
 }
