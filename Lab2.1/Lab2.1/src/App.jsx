aea13c2a5f394f1c273c5f7e3d915beda40b2bef
Amirkhanov Daulet <amirkhanov.dauka.azino@mail.ru>
2026-02-27 22:55:18 +0500

    add Lab2.1 task1

---
+++ Lab2.1/Lab2.1/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import FragmentLayout from "./FragmentLayout";
+import './App.css'
+
+function App() {
+  return <FragmentLayout />;
+}
+
+export default App;
---
+++ Lab2.1/Lab2.1/src/FragmentLayout.jsx
@@ -0,0 +1,11 @@
+function FragmentLayout() {
+    return (
+        <>
+          <header><h1>FragmentLayout</h1></header> 
+          <main><p>This Layout is built using only fragments</p></main>
+          <footer><p>Lab2.1</p></footer>
+        </>
+    );
+}
+
+export default FragmentLayout;
