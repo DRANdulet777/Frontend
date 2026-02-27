fa44f468ea780eb124c14190ec80d678e7503866
Amirkhanov Daulet <amirkhanov.dauka.azino@mail.ru>
2026-02-27 23:07:26 +0500

    add task2 ItemList

--- Lab2.1/Lab2.1/src/App.jsx
+++ Lab2.1/Lab2.1/src/App.jsx
@@ -3,9 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import FragmentLayout from "./FragmentLayout";
 import './App.css'
+import ItemList from './ItemList';
 
 function App() {
-  return <FragmentLayout />;
+  return (
+    <>
+    <FragmentLayout />
+    <ItemList />
+    </>
+  );
 }
 
 export default App;
